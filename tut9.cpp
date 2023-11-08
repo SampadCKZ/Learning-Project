4a5c25ba1e59b94056d1eefdc7f9b3654d20c647
Sampad Chakma <148656909+SampadCKZ@users.noreply.github.com>
2023-11-09 02:37:26 +0600

    String Variable

---
+++ tut9.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char name[14] = "Sampad Chakma";//There are 13 charracters in "Hello World" but I am using 14 because we have to keep one extra for string terminator
+    cout<<"My name is : "<<name;
+    return 0;
+}
