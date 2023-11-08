af3b8351ccd7680d79c01487df3ed700f6818625
Sampad Chakma <148656909+SampadCKZ@users.noreply.github.com>
2023-11-09 02:35:43 +0600

    Datatype and Variables

---
+++ tut8.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int a=14, b=15;
+    float pi=3.14;
+    char c ='d';
+    bool is_true = false;
+    cout<<"Number1 = "<<a<<endl<<"Number2 = "<<b<<endl;
+    cout<<"value of pi is : "<<pi<<endl;
+    cout<<"Charracter = "<<c<<endl;
+    cout<<"The value is : "<<is_true;
+
+    return 0;
+}
+
