4390d0ce032a2ab562c2f00550f55eafd17202de
Sampad Chakma <148656909+SampadCKZ@users.noreply.github.com>
2023-11-09 02:32:52 +0600

    New line

---
+++ tut5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World 1\n";//To start a new line \n
+    cout<<"Hello World 2"<<endl;//Also we can use <<endl to start a new line
+    cout<<"Hello World 3"<<endl<<"Hello World 4";
+    return 0;
+}
